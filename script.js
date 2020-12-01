0b616b3d89a3033569bfab52d94f7c38cbf7ba94
EJostad <eric.jostad@outlook.com>
2020-11-30 23:39:59 -0800

    Updated js file to begin the countdown timer once Begin Quiz button is clicked.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var timerEl=document.getElementById("countdown")
+var startButton=document.querySelector("#startButton");
+var timerEl=document.getElementById("countdown");
+
+
+startButton.onclick=()=>{
+  quizTimer();
+}
 
 function quizTimer() {
-    var timeLeft = 30;
+    var timeLeft=10;
   
-    var timeInterval = setInterval(function() {
-      timerEl.textContent = timeLeft + " seconds remain";
+    var timeInterval=setInterval(function() {
+      timerEl.textContent=timeLeft + " seconds remain";
       timeLeft--;
   
-      if (timeLeft === 0) {
-        timerEl.textContent = "Time's up!";
+      if (timeLeft===0) {
+        timerEl.textContent= "Time's up!";
         clearInterval(timeInterval);
       }
   
     }, 1000);
   }
-
-  quizTimer();
