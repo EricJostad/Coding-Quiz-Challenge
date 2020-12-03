8343b21e9613a62692e1cf1594158fea9af5a474
EJostad <eric.jostad@outlook.com>
2020-12-03 00:11:58 -0800

    Refactored html, css, and js files. Commented out previous code due to non functionality, but kept for now as backup.

--- script.js
+++ script.js
@@ -1,69 +1,112 @@
-var startButton=document.querySelector("#startButton");
-var questions=document.querySelector("#quizQuestions");
-var cardText=document.querySelector(".card-text");
-var cardTitle=document.querySelector(".card-title");
-var choices=Array.from(document.querySelectorAll("#question"));
-var timerEl=document.querySelector("#countdown");
 
-startButton.onclick=()=>{
-  quizTimer() 
-}
 
-function quizTimer() {
-    var timeLeft=10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Previous code below. Saved for reference, for now.
+
+// var startButton=document.querySelector("#startButton");
+// var questions=document.querySelector("#quizQuestions");
+// var cardText=document.querySelector(".card-text");
+// var cardTitle=document.querySelector(".card-title");
+// var timerEl=document.querySelector("#countdown");
+// var currentIndex = 0;
+
+
+// function quizTimer() {
+//     var timeLeft=10;
   
-    var timeInterval=setInterval(function() {
-      timerEl.textContent=timeLeft + " seconds remain";
-      timeLeft--;
+//     var timeInterval=setInterval(function() {
+//       timerEl.textContent=timeLeft + " seconds remain";
+//       timeLeft--;
   
-      if (timeLeft===0) {
-        timerEl.textContent= "Time's up!";
-        clearInterval(timeInterval);
-      }
+//       if (timeLeft===0) {
+//         timerEl.textContent= "Time's up!";
+//         clearInterval(timeInterval);
+//       }
   
-    }, 1000);
-  }
+//     }, 1000);
+//   }
 
-startButton.addEventListener("click", function(){
+// startButton.addEventListener("click", function(){
 
-  startButton.style.display="none";
+//   // startButton.style.display="none";
   
-  cardText.style.display="none";
+//   // cardText.style.display="none";
 
+  
 
-});
+// });
 
-startButton.addEventListener("click", function(){
-  cardTitle.innerHTML= (question[0].question);
+// startButton.addEventListener("click", function(){
 
+//   getQuestion()
+//   quizTimer()
 
-});
+// });
 
-
-
-
-var currentQuestion = {}
-var correctAnswers = true
-var score = 0
-var questionCounter = 0
-var availableQuestions = []
-
-var question = [
-  {
-    question: "I am question 1.",
-    choice1: "I am answer 1",
-    choice2: "I am answer 2",
-    choice3: "I am answer 3",
-    choice4: "I am answer 4",
-    answer: 2
-  }]
+// var question = [
 //   {
 //     question: "I am question 1.",
-//     choice1: "I am answer 1",
-//     choice2: "I am answer 2",
-//     choice3: "I am answer 3",
-//     choice4: "I am answer 4",
-//     answer: 2
+//     choices: ["I am answer 1", "I am answer 2", "I am answer 3", "I am answer 4"],
+//     answer: "I am answer 2"
+//   },
+//   {
+//     question: "I am question 2.",
+//     choices: ["I am answer 1", "I am answer 2", "I am answer 3", "I am answer 4"],
+//     answer: "I am answer 3"
 //   }
 // ]
 
+// function getQuestion(){
+
+//   var currentQuestion = question[currentIndex]
+//   var questionTitle = document.getElementById("question")
+//   questionTitle.textContent = currentQuestion.question
+
+//   currentQuestion.choices.forEach(function(choice){
+    
+//   var createButton = document.createElement("button")
+
+//   createButton.setAttribute("class", "buttons" )
+//   createButton.setAttribute("value", choice)
+//   createButton.textContent = choice
+//   createButton.onclick = currentQuestion++
+//   questions.append(createButton)
+
+// });
+// }
