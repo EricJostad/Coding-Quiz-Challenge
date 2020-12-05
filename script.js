b147441395f4b4f468d3e8fc81376cdc309e3d90
EJostad <eric.jostad@outlook.com>
2020-12-04 21:36:17 -0800

    Updated css filing a bit more as well as updated the comments in the js file.

--- script.js
+++ script.js
@@ -36,43 +36,42 @@ var quizQuestions = [
     }
 ];
 
-//status keeper and rules for QUIZ game (see functions list near bottom)
+//Status keeper and rules for QUIZ game (see functions list near bottom)
 var quizStatus = {};
-//this is used at end-of-game
+//This is used at end-of-game
 resetQuizStatus();
 
-//get local history, then check if null (null=never played)
+//Get local history, then check if null (null=never played)
 var localHistory = JSON.parse(localStorage.getItem('localHistory'))
 //JSON Stringify to store un-set item, then get the item and un-stringify
 if (localHistory === null) {
-    //default values if no local storage exists
+    //Default values if no local storage exists
     localHistory = [
-        ['Erik', 50],
-        ['Justin', 45],
-        ['Mishka', 40],
-        ['Rasputin' , 35],
+        ['Chief', 117],
+        ['Johnny', 90],
+        ['Terry', 50],
         ['Dummy', 0]
     ];
     localStorage.setItem('localHistory', JSON.stringify(localHistory));    
 } 
 localHistory = JSON.parse(localStorage.getItem('localHistory'))
-//a copy used as reference when creating a new high-score table so it can write and refer to old scores simultaneously
+//A copy used as reference when creating a new high-score table so it can write and refer to old scores simultaneously
 var oldLocalhistory = JSON.parse(localStorage.getItem('localHistory'))
 
-//on-load page element set-ups
-    //show-last-score button
+//On-load page element set-ups
+    //Show-last-score button
     var showLastscores = document.createElement("section")
     showLastscores.id = "last-score-button";
     document.body.appendChild(showLastscores);
     showLastscores.innerText = "Click here to show High Scores"
     showLastscores.addEventListener("click", showHighscores);
 
-    //set up canvas where game is played
+    //Set up canvas where game is played
     var quizCanvas = document.createElement("main");
     quizCanvas.id = "quiz-area";
     document.body.appendChild(quizCanvas);
 
-    //timer / score
+    //Timer / score
     var timerCounter = document.createElement("h2");
     timerCounter.id = "timer";
     quizCanvas.appendChild(timerCounter);
@@ -81,21 +80,21 @@ var oldLocalhistory = JSON.parse(localStorage.getItem('localHistory'))
     var quizQuestion = document.createElement("p");
     quizQuestion.id = "quiz-question";
     quizCanvas.appendChild(quizQuestion);
-    //initial "intro" text
+    //Initial "intro" text
     quizQuestion.textContent = quizQuestions[0].question;
 
-    //creates answer p-tags where answers are shown
+    //Creates answer p-tags where answers are shown
     for (var i = 0; i < quizQuestions[0].choices.length; i++) {
         var answerEl = document.createElement("p")
         answerEl.id = "answer-" + (i + 1);
         answerEl.className = "quiz-answer";
         quizCanvas.appendChild(answerEl);
         answerEl.textContent = quizQuestions[0].choices[i];
-        //onclicks applied that check if answer is correct then itterates to next quizStatus
+        //Onclicks applied that check if answer is correct then itterates to next quizStatus
         answerEl.addEventListener('click', answerClick);    
     }    
 
-    //creates status reproter area, just lets user know if the last question was correct or not
+    //Creates status reproter area, just lets user know if the last question was correct or not
     var answerResult = document.createElement("p");
     answerResult.id = "answer-result";
     quizCanvas.appendChild(answerResult);
@@ -104,13 +103,13 @@ var oldLocalhistory = JSON.parse(localStorage.getItem('localHistory'))
     answerResult.textContent = "";
     var startGamebutton = document.createElement("button");
 
-    //start-game button
+    //Start-game button
     startGamebutton.id = "game-start-button";
     startGamebutton.innerText = "Begin Quiz";
     quizCanvas.appendChild(startGamebutton);
     startGamebutton.addEventListener("click", preGame);
 
-    //userform for name entry
+    //Userform for name entry
     var userNameform = document.createElement("FORM");
     userNameform.id = "user-name-form";
     quizCanvas.appendChild(userNameform);  
@@ -123,63 +122,63 @@ var oldLocalhistory = JSON.parse(localStorage.getItem('localHistory'))
     userNamebutton.id = "user-name-button";
     userNamebutton.innerText = "Submit"
     quizCanvas.appendChild(userNamebutton);
-    //user only sees this if they "win" game.
+    //User only sees this if they "win" game.
     userNamebutton.addEventListener("click", gameWin);
 //Page element set-up complete
 
-//functions for game
+//Functions for game
 
-//pre-game count-down that shows the game-rules/concept to user.
+//Pre-game count-down that shows the game-rules/concept to user.
 function preGame() {
-    //hides game-starting button and sets it up for end-game style
+    //Hides game-starting button and sets it up for end-game style
     startGamebutton.style.visibility = "hidden";
     startGamebutton.id = "game-started-button";    
     startGamebutton.innerText = "Click HERE to try again!"
-    //shows user the game rules and sets the pre-game timer        
+    //Shows user the game rules and sets the pre-game timer        
     answerResult.textContent = "You will have 60 seconds to answer all the questions in the quiz. The game will begin...";
     timerCounter.textContent = quizStatus.preGametimer;
-    //this triggers for the sake of a re-initiation of the game on the same page-load
+    //This triggers for the sake of a re-initiation of the game on the same page-load
     for (var i = 0; i < 4; i++) {                
         answerEl = document.getElementById("answer-" + (i + 1));
         answerEl.textContent = "";
     }
-    //reveal counter
+    //Reveal counter
     document.getElementById("timer").style = "color: black;"
-    //this is a var so that the interval can be stopped with clearInterval    
+    //This is a var so that the interval can be stopped with clearInterval    
     var preGamegame = setInterval(function() {        
         quizStatus.preGametimer--;
         timerCounter.textContent = quizStatus.preGametimer;
         if (quizStatus.preGametimer === 1) {
             answerResult.textContent = "Now!";            
         }  else if (quizStatus.preGametimer < 1) {
-            //game is about to start
-            //stop pregame interval
+            //Game is about to start
+            //Stop pregame interval
             clearInterval(preGamegame);        
-            //set answer result to in-game style
+            //Set answer result to in-game style
             answerResult.textContent = "";
             answerResult.style.textAlign = "right";
             answerResult.style.fontSize = "20px";
             answerResult.textContent = "";
-            //set counter to start time main game
+            //Set counter to start time main game
             timerCounter.textContent = quizStatus.gameLength;
-            //set first question
+            //Set first question
             questionAsked()
-            //start game
+            //Start game
             mainGame(); 
         }
     }, 1000);
 };
 
-//main game of page
+//Main game of page
 function mainGame () {
-    //each inteval checks game status to refresh question or if game is over.
+    //Each inteval checks game status to refresh question or if game is over.
     var timerGame = setInterval(function() {
-        //quesiton updator
+        //Quesiton updator
         questionAsked();
-        //updates timer every tick
+        //Updates timer every tick
         timerCounter.textContent--;
-        //end-game checkers here.
-            //time-up event. If time ends (<1), trigger "game over" endgame.
+        //End-game checkers here.
+            //Time-up event. If time ends (<1), trigger "game over" endgame.
             if (timerCounter.textContent < 1) {
                 gameLose(timerGame);
             } else if (quizStatus.questionNum > quizQuestions.length - 2) {
@@ -189,13 +188,13 @@ function mainGame () {
 }
 
 function questionAsked() {
-     //this updates the question if the game-status is set to False (which occurs after user selects an answer)
+     //This updates the question if the game-status is set to False (which occurs after user selects an answer)
      if (!quizStatus.questionAsked) {
-        //this loop updates the question, so first thing: set "asked" = true, it's set to false when user clicks on answer
+        //This loop updates the question, so first thing: set "asked" = true, it's set to false when user clicks on answer
         quizStatus.questionAsked = true;
-        //update question
+        //Update question
         quizQuestion.textContent = quizQuestions[quizStatus.questionNum].question;
-        //update answers, loop through array for each answer element. It's locked at 4 so this is not dynamic.
+        //Update answers, loop through array for each answer element. It's locked at 4 so this is not dynamic.
         for (var i = 0; i < 4; i++) {
             answerEl = document.getElementById("answer-" + (i + 1));
             answerEl.textContent = quizQuestions[quizStatus.questionNum].choices[i];
@@ -205,26 +204,26 @@ function questionAsked() {
 }
 
 function gameLose(timerGame) {
-    //stops game
+    //Stops game
     clearInterval(timerGame);
-    //update play area to show high scores and sad alerts
+    //Update play area to show high scores and sad alerts
     quizQuestion.textContent = "You're out of time!"
     answerResult.style.textAlign = "center";
     answerResult.style.fontSize = "40px";
     answerResult.textContent = "Click HERE to try again!";
     timerCounter.textContent = '';
-    //load any stored data
+    //Load any stored data
     for (var i = 0; i < 4; i++) {                
         answerEl = document.getElementById("answer-" + (i + 1));
         answerEl.textContent = quizQuestions[6].choices[i] + localHistory[i][0] + " " + localHistory[i][1];
     }
-    //set the game status back to default for next play
+    //Set the game status back to default for next play
     quizCanvas.addEventListener("click", preGame);            
     resetQuizStatus();
 };
 
 function getUsername(timerGame) {
-    //stops game, gets name of User
+    //Stops game, gets name of User
     clearInterval(timerGame);
     for (var i = 0; i < 4; i++) {                
         answerEl = document.getElementById("answer-" + (i + 1));
@@ -238,53 +237,53 @@ function getUsername(timerGame) {
     userNamebutton.style.visibility = "visible";
 }
 
-//game success
+//Game success
 function gameWin() {
-    //hides user-form
+    //Hides user-form
     userNameform.style.visibility = "hidden";
     userNamebutton.style.visibility = "hidden";
     quizStatus.enteredName = userNameenter.value;
-    //update status to show previous high-scores in next steps
+    //Update status to show previous high-scores in next steps
     quizQuestion.textContent = quizQuestions[6].question;
-    //game status alert update
+    //Game status alert update
     answerResult.textContent = "The game is over!";
-    //this variable helps maintain continuity between the old records and new records. It also prevents first "if" to fire more than once.
+    //This variable helps maintain continuity between the old records and new records. It also prevents first "if" to fire more than once.
     var newRecord = 0;                
-    //update answers to high scores and create new local history
+    //Update answers to high scores and create new local history
     for (var i = 0; i < 4; i++) {  
         answerEl.style.visibility = "visible";                             
-        //check if new record
+        //Check if new record
         if (timerCounter.textContent > localHistory[i][1] && newRecord === 0) {
-            //only prompts when a high score happens. Nice work!
+            //Only prompts when a high score happens. Nice work!
             answerResult.textContent = "You got a new record!";   
             answerEl = document.getElementById("answer-" + (i + 1));                 
             answerEl.textContent = quizQuestions[6].choices[i] + quizStatus.enteredName + " " + timerCounter.textContent;
-            //here's that checker to stop this path from happening twice
+            //Here's that checker to stop this path from happening twice
             newRecord++;
-            //logging new record into localHistory
+            //Logging new record into localHistory
             localHistory[i][0] = quizStatus.enteredName;
             localHistory[i][1] = timerCounter.textContent;
             answerEl.className = "quiz-answer quiz-answer-highscore";
 
         } else {
-            //oldLocalhistory used as "memory" as localHistory gets overwritten in if script
+            //OldLocalhistory used as "memory" as localHistory gets overwritten in if script
             answerEl = document.getElementById("answer-" + (i + 1));
             answerEl.textContent = quizQuestions[6].choices[i] + oldLocalhistory[i - newRecord][0] + " " + oldLocalhistory[i - newRecord][1];
             localHistory[i][0] = oldLocalhistory[i - newRecord][0];
             localHistory[i][1] = oldLocalhistory[i - newRecord][1];
         }
     }
-    //show user their score
+    //Show user their score
     timerCounter.textContent = quizStatus.enteredName + "'s Final Score: " + timerCounter.textContent;
-    //save to local storage high scores                
+    //Save to local storage high scores                
     localStorage.setItem('localHistory', JSON.stringify(localHistory));
-    //reset for next game
+    //Reset for next game
     resetQuizStatus();
-    //reveals game-starting button
+    //Reveals game-starting button
     startGamebutton.style.visibility = "visible";
 };
 
-//loops through and shows the current high scores
+//Loops through and shows the current high scores
 function showHighscores() {
     startGamebutton.id = "game-started-button";
     for (var i = 0; i < 4; i++) {                
@@ -293,7 +292,7 @@ function showHighscores() {
     }
 }
 
-//default settings reset
+//Default settings reset
 function resetQuizStatus() {
     quizStatus =  {
         questionNum: 1,
@@ -306,7 +305,7 @@ function resetQuizStatus() {
     }
 };
 
-//checks if clicked answer is correct then sets up mainGame for next question
+//Checks if clicked answer is correct then sets up mainGame for next question
 function answerClick () {
     if (parseInt(this.id.charAt(this.id.length - 1)) === quizStatus.correctAnswer) {
         this.className = "quiz-answer quiz-answer-correct";
@@ -316,7 +315,7 @@ function answerClick () {
         answerResult.textContent = "Last Question: Wrong!";
         timerCounter.textContent = timerCounter.textContent - quizStatus.timerPunishment;        
     }    
-    //update game status to next question for next iteration
+    //Update game status to next question for next iteration
     quizStatus.questionAsked = false;
     quizStatus.questionNum++;
     quizStatus.correctAnswer = quizQuestions[quizStatus.questionNum].answer;
