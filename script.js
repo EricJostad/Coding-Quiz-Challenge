5316c5ae6fdf1b75c7240c7194193e594158d8d4
EJostad <eric.jostad@outlook.com>
2020-12-21 22:32:29 -0800

    Updated class names using options tp choices. Added array of objects for quiz questions and answers in js file.

---
+++ script.js
@@ -0,0 +1,28 @@
+// Array of Quiz Questions
+var quiz = [
+    {
+        question: "Question 1",
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+        answer: 0
+    },
+    {
+        question: "Question 2",
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+        answer: 1
+    },
+    {
+        question: "Question 3",
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+        answer: 2
+    },
+    {
+        question: "Question 4",
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+        answer: 3
+    },
+    {
+        question: "Question 5",
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+        answer: 4
+    },
+]
